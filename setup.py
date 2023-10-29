32315256af1c218a2febc071dae3a64c0cd358dd
Aziza Aripova <azizaaripova@Azizas-MacBook-Pro.local>
2023-10-29 15:35:36 -0400

    First commit Packagefiles

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='Book_lover',
+	version='0.1',
+	description="A python package for book lovers",
+	url='https://github.com/azizaar3/book_lover',
+	author='Aziza Aripova',
+	author_email='aziza.aripova30@gmail.com',
+	license='MIT',
+	package=['book_lover']
+)
+
